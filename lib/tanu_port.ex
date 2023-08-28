e27ad4d558fcdb2813d5a2f12e71e9445500ea24
BlackTanuTanuNinjas1035 <ickmkvo15@gmail.com>
2023-08-28 16:28:30 +0900

    ガード句で分岐しなかったので修正。アドレスは文字列で指定すること

--- lib/tanu_port.ex
+++ lib/tanu_port.ex
@@ -18,7 +18,7 @@ defmodule TanuPort do
     Enum.each(1..254, fn ip ->
       spawn(fn ->
         case System.cmd("ping", ["-c", "1", "#{network_ip}.#{ip}"]) do
-          {_, 0}  ->  send(pid, {:child, '#{network_ip}.#{ip}'})
+          {_, 0}  ->  send(pid, {:child, "#{network_ip}.#{ip}"})
           _       ->  {}
         end
       end)
@@ -39,38 +39,48 @@ defmodule TanuPort do
 
   @doc """
   ホストが指定したポートを開放されているか検出します。
-  ipは「''」文字リストにすること、portは整数で
   """
-  def port_scanner(empty_ip_list, _) when empty_ip_list == [] do
-    IO.puts("IP list is empty.")
-  end
-
   def port_scanner(ip_list, port_list) when is_list(ip_list) and is_list(port_list) do
     Enum.each(ip_list, fn ip ->
       Enum.each(port_list, fn port ->
-        scanner(ip, port)
+        String.to_charlist(ip)
+        |> scanner(port)
       end)
     end)
   end
 
   def port_scanner(ip_list, port) when is_list(ip_list) do
     Enum.each(ip_list, fn ip ->
-      scanner(ip, port)
+      String.to_charlist(ip)
+      |> scanner(port)
     end)
   end
 
-  def port_scanner(ip, port) do
-    scanner(ip, port)
+  def port_scanner(ip, port_list) when is_list(port_list) do
+    Enum.each(port_list, fn port ->
+      String.to_charlist(ip)
+      |> scanner(port)
+    end)
+  end
+
+  def port_scanner(ip, port)  do
+    String.to_charlist(ip)
+    |> scanner(port)
   end
 
   defp scanner(ip, port) do
     case :gen_tcp.connect(ip, port, [:binary, active: false]) do
       {:ok, socket} ->
-        IO.puts("#{ip}\t: Opend port #{port}")
+        IO.puts("#{ip}\t: Opened port #{port}")
         :gen_tcp.close(socket)
       {:error, _} ->
         IO.puts("#{ip}\t: Closed port on #{port}")
     end
+  end
+
+  def benchmark() do
+    {time, _result} = :timer.tc(fn -> port_scanner("192.168.0.1", 22) end)
+    IO.puts("Execution time: #{time} microseconds")
   end
 
 end
