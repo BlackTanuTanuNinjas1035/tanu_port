f61782de3270332580d6ed41bf7401dc001f36c4
BlackTanuTanuNinjas1035 <ickmkvo15@gmail.com>
2023-08-28 17:08:35 +0900

    誤字修正＆Flow化。

--- lib/tanu_port.ex
+++ lib/tanu_port.ex
@@ -41,45 +41,59 @@ defmodule TanuPort do
   ホストが指定したポートを開放されているか検出します。
   """
   def port_scanner(ip_list, port_list) when is_list(ip_list) and is_list(port_list) do
-    Enum.each(ip_list, fn ip ->
-      Enum.each(port_list, fn port ->
+    ip_list
+    |> Flow.from_enumerable()
+    |> Flow.map(fn ip ->
+      port_list
+      |> Flow.from_enumerable()
+      |> Flow.map(fn port ->
         String.to_charlist(ip)
         |> scanner(port)
       end)
     end)
+    |> Enum.map(fn ip -> IO.puts ip end)
   end
 
   def port_scanner(ip_list, port) when is_list(ip_list) do
-    Enum.each(ip_list, fn ip ->
+    ip_list
+    |> Flow.from_enumerable()
+    |> Flow.map(fn ip ->
       String.to_charlist(ip)
       |> scanner(port)
     end)
+    |> Enum.map(fn ip -> IO.puts ip end)
   end
 
   def port_scanner(ip, port_list) when is_list(port_list) do
-    Enum.each(port_list, fn port ->
+    port_list
+    |> Flow.from_enumerable()
+    |> Flow.map(fn port ->
       String.to_charlist(ip)
       |> scanner(port)
+
     end)
+    |> Enum.map(fn ip -> IO.puts ip end)
   end
 
   def port_scanner(ip, port)  do
     String.to_charlist(ip)
     |> scanner(port)
+    |> IO.puts
   end
 
   defp scanner(ip, port) do
     case :gen_tcp.connect(ip, port, [:binary, active: false]) do
       {:ok, socket} ->
-        IO.puts("#{ip}\t: Opened port #{port}")
         :gen_tcp.close(socket)
+        "#{ip}\t: Opened port #{port}"
+
       {:error, _} ->
-        IO.puts("#{ip}\t: Closed port on #{port}")
+        "#{ip}\t: Closed port on #{port}"
     end
   end
 
   def benchmark() do
-    {time, _result} = :timer.tc(fn -> port_scanner("192.168.0.1", 22) end)
+    {time, _result} = :timer.tc(fn -> port_scanner(TanuPort.ip_sweep("192.168.0"), 22) end)
     IO.puts("Execution time: #{time} microseconds")
   end
 
